afbe1b9596ca0bbb29c64cd090bc3f4f8612b85e
nikulam <miro.markus10@gmail.com>
2021-02-04 13:05:24 +0200

    build

--- part2-puhelinluettelo/src/services/persons.js
+++ part2-puhelinluettelo/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://lit-refuge-09312.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
   return axios.get(baseUrl)
